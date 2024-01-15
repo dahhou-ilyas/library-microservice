22c84ad3208e2b8404228d21d80f7d7f1f46ca62
dahhou ilyas <dahhou.ilyas@gmail.com>
2024-01-16 00:04:26 +0100

    add customer service

---
+++ customers/customers.js
@@ -0,0 +1,16 @@
+const express=require('express');
+const app=express();
+const bodyParser=require('body-parser');
+const mongoose=require('mongoose')
+app.use(bodyParser.json())
+
+mongoose.connect('mongodb://127.0.0.1:27017/customers');
+
+// call customer model
+require('./CustomersModel')
+const Customer=mongoose.model('Customer')
+//
+
+app.listen(3001,()=>{
+    console.log("app listen in port 3001");
+})
