de3ab2a5e999975669f2ec5ceceaf2e9e5d5f4f7
dahhou ilyas <dahhou.ilyas@gmail.com>
2024-01-15 21:20:55 +0100

    create model books

--- books/books.js
+++ books/books.js
@@ -5,8 +5,9 @@ const mongoose=require('mongoose');
 
 mongoose.connect('mongodb://127.0.0.1:27017/books');
 
-
-
+app.post('/books',(req,res)=>{
+    
+})
 
 app.listen(3000,()=>{
     console.log('server runnign in port 3000');
